1b08d8d4612494c63eed4c8ee2319a037395ae6e
Bruja <sebaspe12345@gmail.com>
2020-06-17 16:33:40 -0500

    update 3

--- src/ConectionDB/connectionMongo.js
+++ src/ConectionDB/connectionMongo.js
@@ -1,18 +1,18 @@
 const mongoose = require('mongoose');
 
 
-/*mongoose.connect('mongodb+srv://Bruja:1925BscJp@cluster0-hp4pe.mongodb.net/Investigacion?retryWrites=true&w=majority', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-    .then(db => { console.log('conected to mongo') })
-    .catch(err => { console.error('Error the connection ', err) })
-*/
-mongoose.connect('mongodb://localhost:27017/SistemaInvestigacion', {
+mongoose.connect('mongodb+srv://Bruja:1925BscJp@cluster0-hp4pe.mongodb.net/Investigacion?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
     .then(db => { console.log('conected to mongo') })
     .catch(err => { console.error('Error the connection ', err) })
 
-module.exports = mongoose
+/*mongoose.connect('mongodb://localhost:27017/SistemaInvestigacion', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(db => { console.log('conected to mongo') })
+    .catch(err => { console.error('Error the connection ', err) })
+
+module.exports = mongoose*/
