6018a24af41864d788613a3a9979edf369a7eb1d
Bruja <sebaspe12345@gmail.com>
2020-06-21 18:26:41 -0500

    export

--- src/ConectionDB/connectionMongo.js
+++ src/ConectionDB/connectionMongo.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
 
-/*mongoose.connect('mongodb+srv://Bruja:1925BscJp@cluster0-hp4pe.mongodb.net/Investigacion?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://Bruja:1925BscJp@cluster0-hp4pe.mongodb.net/Investigacion?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
     .then(db => { console.log('conected to mongo') })
     .catch(err => { console.error('Error the connection ', err) })
-*/
+/*
 
 mongoose.connect('mongodb://localhost:27017/SistemaInvestigacion', {
     useNewUrlParser: true,
@@ -16,4 +16,4 @@ mongoose.connect('mongodb://localhost:27017/SistemaInvestigacion', {
     .then(db => { console.log('conected to mongo') })
     .catch(err => { console.error('Error the connection ', err) })
 
-module.exports = mongoose
+module.exports = mongoose*/
